1daa0e03b50c94743b890befdafb8e39dc3ca234
fitriamelia150 <fitriamelia000979@gmail.com>
2024-03-27 11:21:06 +0700

    push sisanya

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const btn_dice = document.querySelector(".dice");
+const id_advice = document.querySelector(".idAdvice");
+const text_advice = document.querySelector(".textAdvice");
+
+btn_dice.addEventListener("click", getAdvice);
+
+async function getAdvice() {
+	const response = await fetch("https://api.adviceslip.com/advice");
+	const adviceRes = await response.json();
+	id_advice.innerHTML = adviceRes.slip.id;
+	text_advice.innerHTML = '"'+adviceRes.slip.advice+'"';
+
+}
