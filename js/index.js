4165c4adbf583d7db2ede38866fb4bc2586eeec4
fitriamelia150 <fitriamelia000979@gmail.com>
2024-03-28 10:01:20 +0700

    tambahan window load supaya quotes muncul setelah halama ter load tanpa klik button dice

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const btn_dice = document.querySelector(".dice");
 const id_advice = document.querySelector(".idAdvice");
 const text_advice = document.querySelector(".textAdvice");
 
+window.addEventListener("load", getAdvice);
 btn_dice.addEventListener("click", getAdvice);
 
 async function getAdvice() {
